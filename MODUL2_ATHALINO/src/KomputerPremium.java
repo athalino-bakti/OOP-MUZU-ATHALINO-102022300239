7a27a62237e8554c135b0da73a9d25d91658ad6e
athalino-bakti <athalino71@gmail.com>
2024-11-14 11:26:34 +0700

    moodul 2

---
+++ MODUL2_ATHALINO/src/KomputerPremium.java
@@ -0,0 +1,50 @@
+//NAUFAL ATHALINO BAKTI
+// 102022300239
+//SI-47-09
+
+public class KomputerPremium extends Komputer {
+    // To do: Buatlah 1 variable sesuai ketentuan
+    protected boolean ruangPrivat;
+
+    // To do: Buatlah constructor pada class KomputerPremium
+    public KomputerPremium(int jumlahKomputer, String namaWarnet, float hargaPerJam, boolean ruangPrivat) {
+        super(jumlahKomputer, namaWarnet, hargaPerJam);
+        this.ruangPrivat = ruangPrivat;
+    }
+
+    // To do: Buatlah Method Informasi memakai Polymorphism Override dengan isi yang sesuai dengan ketentuan 
+    // (boleh berbeda dengan output jurnal tetapi dengan cangkupan masih sesuai oleh materi modul!)
+    @Override
+    public void Informasi() {
+        super.Informasi();
+        System.out.println("Status          : " + (ruangPrivat ? "Ruang Premium" : "Ruang Standar"));
+        System.out.println("Fasilitas Ruang " + (ruangPrivat ? "Premium" : "Standar") + ":");
+        if (ruangPrivat) {
+            System.out.println("- Ruangan Ber-AC pribadi");
+            System.out.println("- Sofa gaming exclusive");
+            System.out.println("- Komputer spesifikasi tinggi");
+            System.out.println("- Koneksi internet dedicated 100Mbps");
+        } else {
+            System.out.println("- Ruangan gerah");
+            System.out.println("- Sofa majapahit");
+            System.out.println("- Komputer spesifikasi rendah");
+            System.out.println("- Koneksi internet dedicated 1Mbps");
+        }
+    }
+
+
+    // To do: Buatlah method Pesan sesuai dengan ketentuan
+    public void Pesan(int nomorKomputer) {
+        System.out.println("Memesan komputer nomor: " + nomorKomputer);
+    }
+
+    // To do: Buatlah method TambahLayanan sesuai dengan ketentuan
+    public void TambahLayanan(String makanan) {
+        System.out.println("Menambah layanan makanan : " + makanan);
+    }
+
+    // To do: Buatlah method TambahLayanan memakai Polymorphism Overloading sesuai dengan ketentuan
+    public void TambahLayanan(String makanan, String minuman) {
+        System.out.println("Menambah layanan makanan : " + makanan + " dan minuman : " + minuman);
+    }
+}
