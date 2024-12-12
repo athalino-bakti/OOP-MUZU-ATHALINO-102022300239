1c191ab8063895aa5a110dc00002f80a136f8fed
athalino-bakti <athalino71@gmail.com>
2024-12-12 11:46:42 +0700

    Jurnal Modul 3

--- TPMODUL3_ATHALINO/src/App.java
+++ TPMODUL3_ATHALINO/src/App.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //NAUFAL ATHALINO BAKTI
 // 102022300239
 // SI-47-09
@@ -24,30 +24,3 @@ public class App extends Application {
         launch(args);
     }
 }
-=======
-//NAUFAL ATHALINO BAKTI
-// 102022300239
-// SI-47-09
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class App extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        // menampilkan login page
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        Scene scene = new Scene(root);
-        primaryStage.setTitle("Login");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
->>>>>>> 6311fd1db632b49f9b9b3c73b51539e811b373cb
